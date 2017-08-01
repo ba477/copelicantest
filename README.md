03af6f65a725421ede1258c969cefb6efe33d7a7
ba477 <bastien.chemasle@hetic.net>
2017-08-01 18:03:03 +0200

    courage bro

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 npm install
 bower install
+gulp
--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,50 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var clean = require('gulp-clean');
 
-
+gulp.task('clean', function() {
+    gulp.src('*')
+        .pipe(clean({force: true}));
+});
+gulp.task('minify-css', function() {
+    var opts = {comments:true,spare:true};
+    gulp.src(['/**/*.css', '!/bower_components/**'])
+        .pipe(minifyCSS(opts))
+        .pipe(gulp.dest('/css'))
+});
+gulp.task('minify-js', function() {
+    gulp.src(['/**/*.js', '!/bower_components/**'])
+        .pipe(uglify({
+            // inSourceMap:
+            // outSourceMap: "app.js.map"
+        }))
+        .pipe(gulp.dest('/js'))
+});
 gulp.task('connect', function () {
     connect.server({
-        root: 'app/',
-        port: 8888
+        root: '',
+        port: 8000
     });
 });
+
+
+// default task
+gulp.task('default',
+    ['connect', 'watch', 'minify-css', 'minify-js']
+);
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
 gulp.task('sass', function(){
-    return gulp.src('src/assets/styles/sass/*.scss')
+    return gulp.src('sass/*.scss')
         .pipe(sass())    // ici on utilise gulp-sass
-        .pipe(gulp.dest('src/assets/styles/css/'))
+        .pipe(gulp.dest('css/'))
 });
 gulp.task('watch', function(){
-    gulp.watch('src/assets/styles/sass/*.scss', ['sass']);
+    gulp.watch('sass/*.scss', ['sass']);
     // autres observations
 });
